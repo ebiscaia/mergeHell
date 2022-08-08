5c80f2d5cf447f7d52dd67bdc6c260a2b9895f64
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-08 13:09:58 +1000

    Changes in feature branch

--- neogit.py
+++ neogit.py
@@ -1,13 +1,12 @@
 # This is a simple file to test some git integration
 
 # This is from main branch. Feature branch to be empty
-Put some text here in the right branch
 
 # This is from feature branch. Keep main branch empty
-Put some text here in the right branch
+This from feature branch
 
 # This is to be kept by both branches. Keep them Filled
-Put some text here in the right branch
+This from feature branch
 
 # This is to be excluded from both branches. Keep them filled
-Put some text here in the right branch
+This from feature branch
