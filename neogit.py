5b4fd423895066131b854252fdb1e2169b511024
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-08 22:37:15 +1000

    File redifined

--- neogit.py
+++ neogit.py
@@ -1,13 +1,25 @@
-# This is a simple file to test some git integration
+# This is a simple program made to create a merge conflict
 
-# This is from main branch. Keep feature branch empty
-This is stil from main branch
+# Function to provide the maximum value of two numbers
+def maxNumber(num1,num2):
 
-# This is from feature branch. Keep main branch empty
-This is from feature branch
 
-# This is to be kept by both branches. Keep them Filled
-This is stil from main branch
-This is from feature branch
 
-# This is to be excluded from both branches. Keep them filled
+
+# Since its inception Docker has been revolutionising the way software
+# is deployed in companies. Developers are taking advantage of the little
+# to no time needed to configure libraries and dependencies [1], the fact
+# that containers are lightweight since they only contain the essential
+# packages to run the application and multiple containers in the same host,
+# which is making Docker the ideal tool for development in all stages of the
+# development cycle. However, to achieve that, simplifications were made
+# and due to that some security concerns were raised. Because Docker
+# containers do not provide kernel isolation [2], a compromised container
+# can also make vulnerable the host OS or the other containers that are being
+# run alongside. Virtual Machines (VMs), on the other hand, where each
+# application is run in its OS and therefore has its own kernel, prevents
+# this type of malware spreading to other applications even when the VMs
+# are sharing the same hardware.
+
+# list of maxNumbers
+def listMaxNumbers(listofNumbers)
