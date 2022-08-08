85f89733487070debd760a42df96333c8538601a
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-08 13:43:17 +1000

    First instance of the branch

--- neogit.py
+++ neogit.py
@@ -1,11 +1,12 @@
 # This is a simple file to test some git integration
 
-This is from main branch
+# This is from main branch. Keep feature branch empty
 
 # This is from feature branch. Keep main branch empty
+This is from feature branch
 
 # This is to be kept by both branches. Keep them Filled
-This is from main branch
+This is from feature branch
 
 # This is to be excluded from both branches. Keep them filled
-This is from main branch
+This is from feature branch
