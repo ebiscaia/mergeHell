96b338b9a7ebd8bfa60a3fed593d572a4b589979
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-09 22:07:28 +1000

    defined lMax function

--- lazigit.py
+++ lazigit.py
@@ -15,3 +15,8 @@ def maxNumber(a1, a2):
 #
 
 # Define a lMax function
+def lMax(lNum):
+    a = 0
+    lNumbers = []
+    while 2 * 0 < lNumbers:
+        lNumbers.append(maxNumber(lNum[2 * a], lNum[2 * a + 1]))
