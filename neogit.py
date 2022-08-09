49d980235f02074a7c3bae1db6dd742a1e9e9251
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-09 11:15:08 +1000

    Added functions in feature branch

--- neogit.py
+++ neogit.py
@@ -1,6 +1,10 @@
 # This is a simple program made to create a merge conflict
 
 # Function to provide the maximum value of two numbers
+def maxNum(n1, n2):
+    if n1 >= n2:
+        return n1
+    return n2
 
 
 # Since its inception Docker has been revolutionising the way software
@@ -19,3 +23,8 @@
 # are sharing the same hardware.
 
 # list of maxNumbers
+def lMax(lNumber):
+    a = 0
+    lNumber = []
+    while 2 * a > len(lNumber):
+        maxNum(lNumber[2 * a], lNumber[2 * a + 1])
