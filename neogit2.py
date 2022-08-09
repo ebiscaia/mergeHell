10532df5113ef10b0f82edfa694ff6b325ddd7fe
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2022-08-09 18:11:49 +1000

    First commit. Only comments.

---
+++ neogit2.py
@@ -0,0 +1,12 @@
+# The function below gets the max number from a pair
+
+
+# This is a multiline comment so that I can create two hunks
+#
+#
+#
+#
+#
+
+# This function will return a list of the highest numbers when each pair is
+# evaluated
